be20962d18ea0565bb8455c2049171811cc24a6e
jongseo0703 <jongseo0703@naver.com>
2025-07-24 11:29:27 +0900

    ✨ Feat : 유저 로그인을 처리하는 UserLoginRequest DTO 생성

---
+++ src/main/java/com/example/login/dto/UserLoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.login.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequest {
+  //아이디
+  private String loginId;
+  //비밀번호
+  private String password;
+
+}
