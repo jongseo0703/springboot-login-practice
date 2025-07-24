74d31b01b5507ccb1c095e2b7edd522f7ed2cae2
jongseo0703 <jongseo0703@naver.com>
2025-07-24 11:12:21 +0900

    ✨ Feat : 유저 엔터티 생성

---
+++ src/main/java/com/example/login/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.login.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User extends BaseEntity {
+  //pk
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long user_id;
+  //이름
+  private String name;
+  //로그인 아이디
+  private String loginId;
+  //비밀번호
+  private String password;
+  //역할
+  @Builder.Default
+  @Enumerated(EnumType.STRING)
+  private Role role = Role.USER;
+}
