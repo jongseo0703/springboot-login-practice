7c6f7e6a93504550120151a8e458a0abe706f7c6
jongseo0703 <jongseo0703@naver.com>
2025-07-24 11:19:55 +0900

    ✨ Feat : 모든 엔터티의 공통 속성을 정의하는 BaseEntity 생성

---
+++ src/main/java/com/example/login/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.login.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  // 생성시간
+  @CreatedDate
+  private LocalDateTime createdAt;
+  // 수정시간
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
