91270404d583e74a3b601deb7453d92728fb9c65
jongseo0703 <jongseo0703@naver.com>
2025-07-24 11:35:26 +0900

    ✨ Feat : 보안 관련 설정 파일인 SecurityConfig 생성

---
+++ src/main/java/com/example/login/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+      .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+    return httpSecurity.build();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
