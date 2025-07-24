492dbe111d23e5dcb73e0a3d5625b83508f8537c
jongseo0703 <jongseo0703@naver.com>
2025-07-24 11:29:03 +0900

    ✨ Feat : 유저 생성을 처리하는 UserCreateRequest DTO 생성

---
+++ src/main/java/com/example/login/dto/UserCreateRequest.java
@@ -0,0 +1,15 @@
+package com.example.login.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateRequest {
+  //아이디
+  private String loginId;
+  //비번
+  private String password;
+  //닉네임
+  private String name;
+}
